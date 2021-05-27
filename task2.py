e66052194eef4229e5c77900f8928c71ed9832e3
Naishal13 <84837362+Naishal13@users.noreply.github.com>
2021-05-27 14:33:18 +0530

    task 2 - basic variable

---
+++ task2.py
@@ -0,0 +1,26 @@
+print ("hello naishal")
+
+#eg-2
+a=10
+b=13
+print (a)
+print ("value of b is ", b)
+
+#eg-3
+a1 = 13
+print(a1, "is of type", a1)
+
+#eg-4
+name= "Naishal Talati"
+print ("Name is ", name)
+print (name [3:6])
+
+#eg-5
+list1 = [13,15,18]
+print (list1)
+print ("list1[2] = ",list1[2])
+
+#eg-6
+d = { 1:'naishal', 2: 'talati'}
+print (type(d))
+print ("d[1] = ", d[1] )
